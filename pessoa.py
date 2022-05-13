f9ca6dda17f97af0387d119e283bb515529574b8
rodrigostiven <rodrigo.stiven@gmail.com>
2022-05-12 23:17:34 -0300

    Adição da biblioteca random nas classes pessoa e pokemon

--- pessoa.py
+++ pessoa.py
@@ -1,12 +1,27 @@
+import random
 from pokemon import *
 
+NOMES = ["João", "Isabela", "Lorena", "Francisco", "Ricardo", "Diego",
+         "Patrícia", "Gustavo", "Gerônimo", "Gary", "Ash", "Brock"
+         ]
+
+POKEMONS = [
+    PokemonFogo("Charmander"),
+    PokemonFogo("Charizard"),
+    PokemonEletrico("Pikachu"),
+    PokemonEletrico("Raichu"),
+    PokemonAgua("Squirtle"),
+    PokemonAgua("Gyarados")
+]
+
 class Pessoa:
+
 
     def __init__(self, nome=None, pokemons=[]):
         if nome:
             self.nome = nome
         else:
-            self.nome = "Anônimo"
+            self.nome = random.choice(NOMES)
 
         self.pokemons = pokemons
 
@@ -14,12 +29,30 @@ class Pessoa:
         return self.nome
 
     def mostrar_pokemons(self):
-        for pokemon in self.pokemons:
-            print(pokemon)
-
+        if self.pokemons:
+            print("Pokemons de {}".format(self))
+            for pokemon in self.pokemons:
+                print(pokemon)
+        else:
+            print("{} não tem nenhum pokemon".format(self))
 class Player(Pessoa):
     tipo = "player"
+
+    def capturar(self, pokemon):
+        self.pokemons.append(pokemon)
+        print("{} capturou {}".format(self, pokemon))
 
 
 class Inimigo(Pessoa):
     tipo = "inimigo"
+
+    def __init__(self, nome=None, pokemons=[]):
+        if not pokemons:
+            for i in range(random.randint(1, 6)):
+                pokemons.append(random.choice(POKEMONS))
+
+        super().__init__(nome=nome, pokemons=pokemons)
+
+meu_inimigo = Inimigo()
+print(meu_inimigo)
+meu_inimigo.mostrar_pokemons()
